42e934c34c467a6c4b43aae1f534f8d7c2f6c069
j-rin <jp2001110@gmail.com>
2022-07-29 21:02:16 +0530

    updated

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,77 +1,85 @@
-const { json } = require("express");
-const asyncHandler =require("express-async-handler");
-const User = require("../models/userModel")
-const generateToken =require ('../config/generateToken')
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
 
-const registerUser =asyncHandler(async( req,res) => {
-    const { name,email,password,pic } = req.body;
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+// const allUsers = asyncHandler(async (req, res) => {
+//   const keyword = req.query.search
+//     ? {
+//         $or: [
+//           { name: { $regex: req.query.search, $options: "i" } },
+//           { email: { $regex: req.query.search, $options: "i" } },
+//         ],
+//       }
+//     : {};
 
+//   const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+//   res.send(users);
+// });
 
-    if (!name ||!email||!password) {
-        res.status(400)
-        throw new Error("Please Enter all the fields");
-    }
+//@description     Register new user
+//@route           POST /api/user/
+//@access          Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
 
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
 
-    const userExist = await User.findOne({email});
-    if (userExist) {
-        res.status(400)
-        throw new Error("User already exist");
-    }
+  const userExists = await User.findOne({ email });
 
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    const user = await User.create ({
-        name,
-        email,
-        password,
-        pic,
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
     });
-
-    if(user) {
-        res.status(201).json({
-
-            id:user._id,
-            name:user,name,
-            emaul:user.email,
-            pic:user.pic,
-            token:generateToken(user._id),
-        });
-    }else{
-
-
-        res.status(400);
-        throw new Erroor ("Failed to create the user");
-    }
-
-
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
 });
 
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
+  const user = await User.findOne({ email });
 
-const authUser = asyncHandler(async (req,res)=>{
-    const {email,password} =req.body;
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
 
-
-    const user = await User.findOne ({email});
-
-
-    if(user && (await user.matchPaassword(password))) {
-        res.json({
-            id:user._id,
-            name:user,name,
-            emaul:user.email,
-            pic:user.pic,
-            token:generateToken(user._id),
-        })
-    }
-    else{
-
-        res.status(401)
-        throw new Error("Inavalid Username or Password");
-    }
-
-
-}
-)
-
-module.exports ={registerUser,authUser};
+module.exports = { registerUser, authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {registerUser,authUser} = require("../controller/userController")
+const { protect } = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
 
-router.route("/").post(registerUser)
-router.route("/login",authUser)
+router.route("/").post(registerUser).get(protect);
+router.post("/login",authUser);
 
-module.exports =router;
+module.exports = router;
